fa593f52999831e55b3ae24ce55482f75b8ca44c
Adam Dangoor <adamdangoor@gmail.com>
2025-03-27 21:26:11 +0000

    Add the start of testing thoroughly

--- src/mypy_strict_kwargs/plugin.py
+++ src/mypy_strict_kwargs/plugin.py
@@ -2,12 +2,29 @@
 ``mypy`` plugin to enforce strict keyword arguments.
 """
 
+import tomllib
 from collections.abc import Callable
+from configparser import ConfigParser
 from functools import partial
+from pathlib import Path
+from typing import Any
 
 from mypy.nodes import ArgKind
+from mypy.options import Options
 from mypy.plugin import FunctionSigContext, MethodSigContext, Plugin
 from mypy.types import CallableType
+
+
+def _parse_toml(config_file: Path) -> dict[str, Any] | None:
+    """Returns a dict of config keys to values.
+
+    Returns ``None`` if the config file is not a TOML file.
+    """
+    if config_file.suffix != ".toml":
+        return None
+
+    with config_file.open(mode="rb") as rf:
+        return tomllib.load(rf)
 
 
 def _transform_signature(
@@ -66,7 +83,7 @@ def _transform_signature(
             continue
 
         # If name is None, it is a positional-only argument; leave it as is
-        if name is None:
+        if name is None or name in __import__(name="builtins").__dict__:
             new_arg_kinds.append(kind)
 
         # Transform positional arguments that can also be keyword arguments
@@ -86,10 +103,62 @@ def _transform_signature(
     return original_sig.copy_modified(arg_kinds=new_arg_kinds)
 
 
+class _KeywordOnlyPluginConfig:
+    """A mypy plugin config holder.
+
+    Attributes:
+        ignore_builtins: Whether to ignore built-in functions.
+    """
+
+    __slots__ = ("ignore_builtins",)
+    ignore_builtins: bool
+
+    def __init__(self, options: Options) -> None:
+        if options.config_file is None:  # pragma: no cover
+            return
+
+        toml_config = _parse_toml(config_file=Path(options.config_file))
+        if toml_config is not None:
+            config = toml_config.get("tool", {})
+            config = config.get("mypy_strict_kwargs", {})
+            breakpoint()
+            for key in self.__slots__:
+                setting = config.get(key, False)
+                if not isinstance(setting, bool):
+                    msg = (
+                        f"Configuration value must be a boolean for key: {key}"
+                    )
+                    raise TypeError(msg)
+                setattr(self, key, setting)
+        else:
+            plugin_config = ConfigParser()
+            plugin_config.read(filenames=options.config_file)
+            for key in self.__slots__:
+                setting = plugin_config.getboolean(
+                    section="mypy-strict-kwargs",
+                    option=key,
+                    fallback=False,
+                )
+                setattr(self, key, setting)
+
+    def to_data(self) -> dict[str, Any]:
+        """Returns a dict of config names to their values."""
+        return {key: getattr(self, key) for key in self.__slots__}
+
+
 class KeywordOnlyPlugin(Plugin):
     """
     A plugin that transforms positional arguments to keyword-only arguments.
     """
+
+    def __init__(self, options: Options) -> None:
+        """Configure the plugin."""
+        super().__init__(options=options)
+        if options.config_file is None:  # pragma: no cover
+            return
+        self._plugin_config = _KeywordOnlyPluginConfig(options=options)
+        self._plugin_data = self._plugin_config.to_data()
+        breakpoint()
 
     def get_function_signature_hook(
         self,
