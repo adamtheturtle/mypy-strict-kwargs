5cf584bcc7b897caa9c4451d9cf75e5e20a26b4d
Adam Dangoor <adamdangoor@gmail.com>
2026-05-20 06:37:06 +0100

    Move non-function type coverage to plugin test (#469)

--- tests/test_no_config_file.py
+++ tests/test_no_config_file.py
@@ -1,9 +1,6 @@
 """Test plugin behavior when no configuration file is provided."""
 
-import importlib
-
 from mypy.options import Options
-from mypy.types import AnyType, TypeOfAny
 
 from mypy_strict_kwargs.plugin import KeywordOnlyPlugin
 
@@ -19,19 +16,3 @@ def test_no_config_file() -> None:
 
     assert plugin.get_function_signature_hook(fullname="test.func") is not None
     assert plugin.get_method_signature_hook(fullname="test.method") is not None
-
-
-def test_transform_type_leaves_non_function_types_unchanged() -> None:
-    """Test that non-function proper types are left unchanged."""
-    typ = AnyType(type_of_any=TypeOfAny.explicit)
-    plugin_module = importlib.import_module(name="mypy_strict_kwargs.plugin")
-    transform_type = vars(plugin_module)["_transform_type"]
-
-    transformed_type = transform_type(
-        typ=typ,
-        fullname="test.value",
-        ignore_names=[],
-        skip_bound_argument=False,
-    )
-
-    assert transformed_type is typ
